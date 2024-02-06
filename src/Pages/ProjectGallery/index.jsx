9dec3d83d9070c95b2401057ac2d12d95f0b23c8
CestSamual <mvf4@hotmail.co.uk>
2024-02-06 09:41:52 +0000

    Add Project Gallery

--- src/Pages/ProjectGallery/index.jsx
+++ src/Pages/ProjectGallery/index.jsx
@@ -1,3 +1,27 @@
 import React from "react";
-function ProjectGallery(){}
+import projects from"../../projects.json"
+import Wrapper from "../../Components/Wrapper";
+import Project from "../../Components/Project";
+function ProjectGallery(){
+    return (
+        <>
+        <div className="project">
+        <h1 className="title">MMyy Prrojjectts</h1>
+        </div>
+    
+          <Wrapper>
+            {projects.map((project) => (
+              <Project
+              name={project.name}
+              key={project.id}
+              image={project.image}
+              github={project.github}
+              deployedapp={project.deployedapp}
+              />
+            ))}
+          </Wrapper>
+        </>
+        
+        );
+}
 export default ProjectGallery

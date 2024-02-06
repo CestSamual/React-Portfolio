099c1af986d20e557935bc63e62b72339b66edb0
CestSamual <mvf4@hotmail.co.uk>
2024-02-06 15:45:03 +0000

    Update

--- src/Components/ContactForm/index.jsx
+++ src/Components/ContactForm/index.jsx
@@ -4,9 +4,9 @@ import Home from "../../Pages/Home";
 
 function ContactForm(){
     
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [formData, setFormData] = useState({ name: '', email: '', message: '' });
     
-  const {name, email, message}  = formState;
+    const {name, email, message}  = formData;
     
     const [errorMessage, setErrorMessage] = useState('');
 
@@ -29,20 +29,19 @@ function ContactForm(){
         }
         
         if (!errorMessage) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        setFormData({...formData, [e.target.name]: e.target.value })
         }
     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
+    const handleSubmit= (event) => {
+        event.preventDefault();
 
-    setFormState({
-        name: "",
-        email: "",
-        message: "",
-    });
-  
-    }
+        setFormData({
+            name: '',
+            email: '',
+            message: '',
+        });
+    };
     return(
         <>
         <h1 className="title">Conntacct Mee</h1>
@@ -65,6 +64,6 @@ function ContactForm(){
     <button type="submit" onSubmit={handleSubmit} class="btn btn-primary">Suubmmit</button>
 </form>
 </div> 
-    </>)
+    </>);
 }
-export default ContactForm
+export default ContactForm;
